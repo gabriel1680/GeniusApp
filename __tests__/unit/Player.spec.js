a040670b9cca659810994faafae82a9e3b8edeac
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-11-06 22:23:08 -0300

    test(player): add describe block

--- __tests__/unit/Player.spec.js
+++ __tests__/unit/Player.spec.js
@@ -1,17 +1,21 @@
 import { Player } from "../../src/engine/Player";
 
-it('should be able to add colors to selected ones', () => {
-  const player = new Player();
-  expect(player.selectedColors).toStrictEqual([]);
-  player.selectColor('green');
-  player.selectColor('yellow');
-  expect(player.selectedColors).toStrictEqual(['green', 'yellow']);
-});
+describe('Player (unit)', () => {
+  
+  it('should be able to add colors to selected ones', () => {
+    const player = new Player();
+    expect(player.selectedColors).toStrictEqual([]);
+    player.selectColor('green');
+    player.selectColor('yellow');
+    expect(player.selectedColors).toStrictEqual(['green', 'yellow']);
+  });
+  
+  it('should be able to reset selected colors', () => {
+    const player = new Player();
+    player.selectColor('green');
+    player.selectColor('yellow');
+    player.clearSelectedColors();
+    expect(player.selectedColors).toStrictEqual([]);
+  });
 
-it('should be able to reset selected colors', () => {
-  const player = new Player();
-  player.selectColor('green');
-  player.selectColor('yellow');
-  player.clearSelectedColors();
-  expect(player.selectedColors).toStrictEqual([]);
 });
